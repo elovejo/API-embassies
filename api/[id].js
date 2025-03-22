4c30871137706fad68d3b41f4607f5cec3d12cb9
elovejo <elovejo@gmail.com>
2025-03-22 14:23:47 -0500

    Update [id].js

--- api/[id].js
+++ api/[id].js
@@ -1,10 +1,13 @@
 import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default async (req, res) => {
   console.log("req.query:", req.query);
   try {
     const id = req.query.id;
-    
+
     if (!id) {
       return res.status(400).json({ error: "Falta el ID de la embajada" });
     }
@@ -15,21 +18,25 @@ export default async (req, res) => {
       headers: {
         "apikey": process.env.SUPABASE_API_KEY,
         "Authorization": `Bearer ${process.env.SUPABASE_API_KEY}`,
-        "Content-Type": "application/json"
-      }
+      },
     });
+
+    console.log("Response Status:", response.status); // Registro del estado de la respuesta
 
     if (!response.ok) {
       const errorText = await response.text();
+      console.error("Error de Supabase:", errorText); // Registro del error de Supabase
       return res.status(response.status).json({ error: errorText });
     }
 
     const data = await response.json();
-    
+
+    console.log("Data from Supabase:", data); // Registro de los datos de Supabase
+
     if (data.length === 0) {
       return res.status(404).json({ error: "Embajada no encontrada" });
     }
-    
+
     res.json(data[0]);
   } catch (error) {
     console.error("Error al obtener embajada:", error);
