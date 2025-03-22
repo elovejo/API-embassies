bec7faba50fc3d06c3dac768ee1f2b4e2de20f46
elovejo <elovejo@gmail.com>
2025-03-22 13:56:37 -0500

    Update embassies.js

--- api/embassies.js
+++ api/embassies.js
@@ -1,6 +1,7 @@
 import fetch from 'node-fetch';
 
 export default async (req, res) => {
+  console.log("req.query:", req.query);
   try {
     const response = await fetch("https://zuztqqbnvridpzfdxldv.supabase.co/rest/v1/embassies", {
       headers: {
